e9488ceedbb611432dc6f8a784bbc1a503340a84
hyde0038 <hyde0038@algonquinlive.com>
2025-03-07 16:09:52 -0500

    Fetch completed

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,15 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() async {
+  var uri = Uri.parse('https://random-data-api.com/api/v2/users?size=12');
+  try {
+    final resp = await http.get(uri);
+    if (resp.statusCode == 200) {
+      var jsonResp = convert.jsonDecode(resp.body) as List<dynamic>;
+      print('User data: $jsonResp');
+    }
+  } catch (err) {
+    print('Failed http request $uri');
+  }
 }
